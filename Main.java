75fe5056dd030aa5962b3f126b0bff148864e898
Jaine Kiefer <jainekiefer013@gmail.com>
2022-03-29 23:28:22 -0300

    master

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Conta cc = new ContaCorrente();
-        Conta poupança = new ContaPoupança();
+        InterfaceConta cc = new ContaCorrente();
+        InterfaceConta poupança = new ContaPoupança();
 
         cc.imprimirExtrato();
         poupança.imprimirExtrato();
